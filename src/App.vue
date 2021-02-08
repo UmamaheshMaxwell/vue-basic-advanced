3883f15c99075508c885ef306bb36a1f7c1842f9
Umamaheswararao Meka <maheshmeka16@gmail.com>
2021-02-08 19:08:31 +0530

    setup the basic repo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>{{greet}}{{name}}{{message}}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      greet: 'Hello ',
+      name: 'Uma, ',
+      message: 'Welcome to VueJS'
+    }
   }
 }
 </script>
